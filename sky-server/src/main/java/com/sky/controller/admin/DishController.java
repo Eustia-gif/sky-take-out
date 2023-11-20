edd6bb04ef140ad42ad0c394634eec26a93407ee
Eustia-gif <2902421824@qq.com>
2023-11-20 15:14:51 +0800

    实现菜品的删除，分页查询

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -1,6 +1,8 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 import io.swagger.annotations.Api;
@@ -8,6 +10,8 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/dish")
@@ -26,6 +30,21 @@ public class DishController {
     public Result save(@RequestBody DishDTO dishDTO){
         log.info("新增菜品：{}",dishDTO);
         dishService.saveWithFlavor(dishDTO);
+        return Result.success();
+    }
+    @GetMapping("/page")
+    @ApiOperation(value = "分页查询菜品")
+    public  Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO){//请求参数是query的方式通过？来传参，不是json格式，不需要注解
+        log.info("分页查询菜品：{}",dishPageQueryDTO);
+        PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
+        return Result.success(pageResult);
+    }
+    @DeleteMapping
+    @ApiOperation(value = "菜品的批量删除")
+    public Result delete(@RequestParam List<Long> ids){// @RequestParam能够接收多个参数，由springmvc动态的解析出来，传入的参数实例1,2,3
+
+        log.info("删除菜品：{}",ids);
+        dishService.deleteBatch(ids);
         return Result.success();
     }
 }
